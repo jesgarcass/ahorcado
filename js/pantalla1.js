51b18c45b6f4afda20d47a7d95d6742af312debd
masacrejesus <69402001+masacrejesus@users.noreply.github.com>
2022-02-11 17:12:07 -0600

    se creo todo

---
+++ js/pantalla1.js
@@ -0,0 +1,39 @@
+var vidas=5;
+var msg = ''; 
+let palabra=["hola"];
+
+document.onkeypress=mostrarInformacionCaracter;
+
+function mostrarInformacionCaracter(evObject) { 
+    var Caracter = String.fromCharCode(evObject.which); 
+    if(evObject.which>96 && evObject.which <123||evObject.which>64 && evObject.which <90){
+        msg =Caracter;
+        var expresion=new RegExp(msg,"i");
+        
+        if(!expresion.test(control.textContent)&&!expresion.test(palabra)){
+            control.innerHTML += msg;
+            vidas--;
+        }else if(expresion.test(palabra)){
+            document.getElementById("con").innerHTML+=msg;
+        }
+        document.getElementById("vidas").innerHTML="vidas "+vidas;
+        if(vidas<=4){
+            base();
+        }
+        if(vidas<=3){
+            cabeza();
+        }
+        if(vidas<=2){
+            brazoD();
+            brazoI();
+        }
+        if(vidas<=1){
+            cuerpo();
+        }
+        if(vidas<=0){
+            pieD();
+            pieI();
+            alert("perdiste");
+        }
+    }       
+}
